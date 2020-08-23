6e672e35619b44f1da875b3464b1addb8a4204b4
ItsPogle <69850965+ItsPogle@users.noreply.github.com>
2020-08-23 19:14:15 +1000

    1.0

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public int itemID;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == GameManager.Instance.PM.gameObject)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                GameManager.Instance.PickupItem(itemID);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    Vector3 moveInput;
+    Vector3 moveVelocity;
+    Rigidbody rBody;
+    public float moveSpeed = 4;
+
+    public Transform hand;
+    public Transform body;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+            moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveVelocity = moveInput * moveSpeed;
+    }
+    void FixedUpdate()
+    {
+        rBody.velocity = moveVelocity;
+    }
+}
